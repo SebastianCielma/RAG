b01d1441d637dc0ce496de7d44b466483444f50c
sebastiancielma <cielmasebastian@gmail.com>
2025-11-05 11:29:45 +0100

    add custom_types

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import uuid
 import os
 import datetime
 from inngest.experimental import ai
+from data_loader import load_and_chunk_pdf, embed_texts
+from vector_db import QdrantStorage
+from custom_types import *
 
 load_dotenv()
 
